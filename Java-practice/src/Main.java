06241f24b0eb8187a4dbc09b49ed6db0d4507caa
gorb6593 <gorb6593@gmail.com>
2023-09-11 22:35:30 +0900

    study

--- Java-practice/src/Main.java
+++ Java-practice/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(123);
+        System.out.println("안녕"+"하세요");
+        System.out.println("안녕"+123);
+        System.out.println("안녕"+10+20);
+
+        int x = 5;
+        System.out.println(x + 5);
     }
 }
