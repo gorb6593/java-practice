28b0118f14b9cf6f27f6b3f736fea6fa77890c77
gorb6593 <gorb6593@gmail.com>
2023-09-12 22:54:44 +0900

    java-today-practice

--- Java-practice/src/Main.java
+++ Java-practice/src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(123);
-        System.out.println("안녕"+"하세요");
-        System.out.println("안녕"+123);
-        System.out.println("안녕"+10+20);
+        byte bt = 123;
 
-        int x = 5;
-        System.out.println(x + 5);
+        System.out.println(bt);
+        System.out.println(Byte.BYTES);
     }
 }
